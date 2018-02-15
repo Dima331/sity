e550237a9fffc3b530ab0a6b8277cd2562676002
Barcov Dima <di_1@inbox.ru>
2018-02-15 15:21:31 +0300

    one page scroll

--- cross_burger.js
+++ cross_burger.js
@@ -1,7 +1,7 @@
 var items = document.getElementById("items"),
     composition = document.getElementsByClassName("composition"),
     compound = document.getElementsByClassName("slideburger__left-compound"),
-    cross = document.querySelectorAll(".slideburger__left-cross");
+    burgers = document.getElementsByClassName("composition__img2");
 var sizee;
 
 sizee = window.innerWidth;
@@ -14,18 +14,18 @@ window.addEventListener('resize', function () {
     }
 });
 items.addEventListener("click", function (e) {
-    if (e.target.classList.contains("slideburger__left-composition") 
-    || e.target.classList.contains("composition__title") 
-    || e.target.classList.contains("composition__img") ) {
-      
+    if (e.target.classList.contains("slideburger__left-composition")
+        || e.target.classList.contains("composition__title")
+        //  || $('.composition__img2').click()) {
+    ) {
         var trigger;
         if (e.target.classList.contains("slideburger__left-composition")) { // если клик по триггеру
             trigger = e.target;
         } else { // значит клик был по тексту и триггер это родительский узел блока с текстом
             trigger = e.target.parentNode;
         }
-       
-       
+
+
         console.log(34565);
         //var trigger = e.target;
         var content = trigger.nextElementSibling;
@@ -40,26 +40,66 @@ items.addEventListener("click", function (e) {
             content.style.display = "flex";
         }
     }
-    if (e.target.classList.contains("slideburger__left-cross")) {
+    /* if (e.target.classList.contains("slideburger__left-cross")) {
+ 
+         /* условие на крестик */
+    //   console.log(8746758);
+    //    var trigger = e.target;
+    //    var item = trigger.parentNode;
+    //var item_2 = item.parentNode;
+    //item_2.style.display = null;
+    //    item.style.display = null;
 
-        /* условие на крестик */
-        console.log(8746758);
-        var trigger = e.target;
-        var item = trigger.parentNode;
-        //var item_2 = item.parentNode;
-        //item_2.style.display = null;
-        item.style.display = null;
-    }
-/*
-    if (e.target.classList.contains("cross__burger")) {
-        
-                /* условие на крестик 
-                console.log(8746758);
-                var trigger = e.target;
-                var item = trigger.parentNode;
-                var item_2 = item.parentNode;
-                item_2.style.display = null;
-        
-}*/
+    /*
+        if (e.target.classList.contains("cross__burger")) {
+            
+                    /* условие на крестик 
+                    console.log(8746758);
+                    var trigger = e.target;
+                    var item = trigger.parentNode;
+                    var item_2 = item.parentNode;
+                    item_2.style.display = null;
+            
+    }*/
 
 });
+
+
+
+$(document).ready(function () {
+
+    $('.slideburger__left-cross').on('click', function (e) {
+        //console.log(9485)
+        console.log($(this).parent());
+        $(this).parent().css("display", "none");
+
+    });
+});
+$(document).ready(function () {
+    $('.composition').on('click', function (e) {
+        $(this).siblings('.compound').css("display", "flex");
+    });
+
+});
+
+/*
+$('.composition').click(function(){
+    alert('Вы нажали на элемент "foo"');
+  });
+*/
+
+/*
+for (var i = 0; i < burgers.length; i++) {
+    burgers.addEventListener("click", function () {
+        console.log(766777);
+    if (sizee >= 951) {
+        console.log(777777);
+        for (i = 0; i < compound.length; i++) {
+            compound[i].style.display = null;
+        }
+    } else if (sizee < 951) {
+        console.log(88888888);
+        content.style.display = "flex";
+    }
+});
+}  */
---
+++ main.js
@@ -0,0 +1,108 @@
+/*
+$(document).on('wheel', e => {
+  console.log(5467);
+      const deltaY = e.originalEvent.deltaY;
+    if(deltaY > 0){
+      console.log('вниз')
+    }
+    if(deltaY < 0){
+      console.log('вверх')
+    }
+});
+*/
+
+const sections = $(".section");
+const display = $(".maincontent");
+let inScroll = false;
+
+
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
+
+const setActiveMenuItem = itemEq => {
+  $('.fixed-menu__item').eq(itemEq).addClass('active2')
+    .siblings().removeClass('active2')
+} 
+
+const performTransition = sectionEq => {
+  const position = `${sectionEq * -100}%`;
+
+  if (inScroll) return;
+  console.log('event');
+  inScroll = true;
+
+  sections
+    .eq(sectionEq)
+    .addClass("active2")
+    .siblings()
+    .removeClass("active2");
+
+  display.css({
+    transform: `translate(0, ${position})`,
+    "-webkit-transform": `translate(0, ${position})`
+  });
+
+  setTimeout(() => {
+    inScroll = false;
+    setActiveMenuItem(sectionEq);
+  }, 1300); // продолжительность анимации + 300ms - потому что закончится инерция
+};
+
+const scrollToSection = direction => {
+  const activeSection = sections.filter(".active2");
+  const nextSection = activeSection.next();
+  const prevSection = activeSection.prev();
+
+  if (direction === "up" && prevSection.length) {
+    performTransition(prevSection.index());
+  }
+
+  if (direction === "down" && nextSection.length) {
+    performTransition(nextSection.index());
+  }
+};
+
+$(document).on({
+  wheel: e => {
+    console.log('event');
+
+    const deltaY = e.originalEvent.deltaY;
+    const direction = deltaY > 0 ? "down" : "up";
+    console.log(deltaY);
+    scrollToSection(direction);
+  },
+  keydown: e => {
+    switch (e.keyCode) {
+      case 40:
+        scrollToSection("down");
+        break;
+
+      case 38:
+        scrollToSection("up");
+        break;
+    }
+  },
+  touchmove: e => e.preventDefault()
+});
+
+
+$('[data-scroll-to]').on('click', e => {
+  e.preventDefault();
+  const target = parseInt($(e.currentTarget).attr('data-scroll-to'));
+  performTransition(target);
+
+})
+
+if (isMobile) {
+  $(document).swipe({
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+      /**
+       * плагин возвращает фактическое...
+       * ...console.log('event');
+       */
+      const scrollDirection = direction === 'down' ? 'up' : 'down';
+      
+      scrollToSection(scrollDirection);
+    }
+  });
+}
