e1429cac35b8a84b1fd429c61c3fd7e7512e28d3
Dima Barkov <di_1@inbox.ru>
2018-02-17 16:08:42 +0300

    form

--- json.js
+++ json.js
@@ -6,16 +6,14 @@ phone.addEventListener("keydown", function (e) {
     let isDigit = false;
     let isDash = false;
     let isControl = false;
-
     if (e.key >= 0 || e.key <= 9) {
         isDigit = true;
     }
-
     if (e.key == '-') {
         isDash = true;
     }
 
-    if (e.key == "ArrowLeft" || e.key == 'ArrowRight' || e.key == 'Backspace'|| e.key == 'Delete') {
+    if (e.key == "ArrowLeft" || e.key == 'ArrowRight' || e.key == 'Backspace' || e.key == 'Delete') {
         isControl = true;
     }
     if (isDigit == false && isDash == false && isControl == false) {
@@ -43,6 +41,8 @@ flove.addEventListener("keydown", function (e) {
 
 var content__message = document.querySelector(".full_r");
 const button_ex = document.querySelector(".messege__buttom");
+
+
 $('#data').on('submit', submitForm);
 
 function submitForm(ev) {
@@ -52,60 +52,34 @@ function submitForm(ev) {
         data = form.serialize(),
         url = form.attr('action'),
         type = form.attr('method');
+        
     console.log(form);
     console.log(data);
+
+
     ajaxForm(form).done(function (msg) {
         var mes = msg.mes,
             status = msg.status;
 
-            document.body.classList.add('stop-scrolling');
-            /*if (content__message.classList.contains('animach')) {
-              content__message.classList.remove('animach');
-            }*/
-           
-            $('#order__message').removeClass("hid");
-           
-           // $('.messege__answer').text("Request failed: " + textStatus);
-
-        /*
-        if (status === 'OK') {
-            form.append('<p class="success">' + mes + '</p>');
-        } else{
-            form.append('<p class="error">' + mes + '</p>');
-        }*/
-    }).fail(function (jqXHR, textStatus) {
-
         document.body.classList.add('stop-scrolling');
-       /* if (content__message.classList.contains('animach')) {
-          content__message.classList.remove('animach');
-        }*/
+        $('#order__message').removeClass("hid");
+    }).fail(function (jqXHR, textStatus) {
+        document.body.classList.add('stop-scrolling');
+
         if (content__message.classList.contains('animach')) {
             content__message.classList.remove('animach');
-          }
-          content__message.classList.add('active2');
+        }
+        content__message.classList.add('active2');
         $('#order__message').removeClass("hid");
         $('.messege__answer').text("Request failed: " + textStatus);
-        
-
-    // alert("Request failed: " + textStatus);
-
-
     });
-
 };
 
-button_ex.addEventListener("click", function (event) {
-      event.preventDefault();
-    //  content__message.classList.remove('active2');
-    //  content__message.classList.add('animach');
-   //   setTimeout(addhidden, 1000);
-   $('#order__message').addClass("hid");
-      document.body.classList.remove('stop-scrolling');
-     /* function addhidden() {
-        content__message.classList.add('hidden');
-        content__message.classList.remove('animach');
-      }*/
-    });  
+    button_ex.addEventListener("click", function (event) {
+        event.preventDefault();
+        $('#order__message').addClass("hid");
+        document.body.classList.remove('stop-scrolling');
+    });
 
 
 
--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-
+    echo 43857;
     $name = $_POST['user-name'];
     $phone = $_POST['user-phone'];
     $street = $_POST['user-street'];
@@ -11,8 +11,7 @@
     $commradioent = $_POST['radio'];
     $checkbox = $_POST['checkbox']; // 1 или null
     $checkbox = isset($checkbox) ? 'Нет' : 'Да';
-
-
+    
     $mail_message = '
     <html>
     <head>
@@ -35,11 +34,58 @@
     </body>
     </html>';
 
+
+
     $headers = "From: Администратор сайта <Di_1@inbox.ru>\r\n".
                 "MIME-Version: 1.0" . "\r\n" .
                 "Content-type: text/html; charset=UTF-8" . "\r\n";
 
-    $mail = mail('Di_1@inbox.ru', 'Заказ', $mail_message, $headers);
+  // $mail = mail('di_1@inbox.ru', 'Заказ', $mail_message, $headers);
+ 
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  
+//  require 'path/to/PHPMailer/src/Exception.php';
+ // require 'path/to/PHPMailer/src/PHPMailer.php';
+//  require 'path/to/PHPMailer/src/SMTP.php';
+   
+   //Load composer's autoloader
+   //require 'vendor/autoload.php';
+   // require "PHPMailer/PHPMailerAutoload.php";
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+   // require 'vendor/autoload.php';
+    
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+    try {
+        //Server settings
+        //$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'dimbar331@gmail.com';                 // SMTP username
+        $mail->Password = 'lnm5ro2hio';                           // SMTP password
+        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 465;                                    // TCP port to connect to
+    
+        //Recipients
+        $mail->setFrom('dimbar331@gmail.com', 'Mailer');
+        $mail->addAddress('di_1@inbox.ru', 'Joe User');     // Add a recipient
+        $mail->addAddress('dimbar331@gmail.com');               // Name is optional
+        $mail->addReplyTo('dimbar331@gmail.com', 'Information');
+    
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = $mail_message;
+        $mail->AltBody = $mail_message;
+    
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    }
 
     $data = [];
 
